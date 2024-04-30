75c732d794fe41cbdeda990436e1f6a455b7c17d
rafael <rafael@golang.pop-os>
2024-04-30 00:14:59 -0500

    Selected only vscode theme

--- init.lua
+++ init.lua
@@ -5,81 +5,42 @@ local Plug = vim.fn['plug#']
 -------------------------------------------------------------------------------
 vim.call('plug#begin')
 -------------------------------------------------------------------------------
--- Plug('vim-airline/vim-airline')
+Plug('itchyny/lightline.vim')
 Plug('nvim-tree/nvim-web-devicons')
 Plug('akinsho/bufferline.nvim')
 Plug('akinsho/toggleterm.nvim')
-
 -- Themes --------------------------------------------------------------------- 
-Plug('https://github.com/junegunn/seoul256.vim.git')
-Plug('folke/tokyonight.nvim')
-Plug('rose-pine/neovim')
+Plug('Mofiqul/vscode.nvim')
 -------------------------------------------------------------------------------
 -- Code Helpers ---------------------------------------------------------------
 Plug('Raimondi/delimitMate')
 Plug('tpope/vim-commentary')
 Plug('mg979/vim-visual-multi')
+Plug('joshdick/onedark.vim')
 -------------------------------------------------------------------------------
 -- Programing Languages Helpers 
-Plug('nvim-treesitter/nvim-treesitter', {['do'] = 'TSUpdate'}) -- Syntax Parser
-Plug('williamboman/mason.nvim')								-- LSP REQUIREMENTS
-Plug('williamboman/mason-lspconfig.nvim')					-- LSP REQUIREMENTS
-Plug('neovim/nvim-lspconfig')								-- LSP REQUIREMENTS
-Plug('hrsh7th/nvim-cmp')									-- LSP REQUIREMENTS
-Plug('hrsh7th/cmp-nvim-lsp')								-- LSP REQUIREMENTS
-Plug('L3MON4D3/LuaSnip')									-- LSP REQUIREMENTS
-Plug('VonHeikemen/lsp-zero.nvim', {['branch'] = 'v3.x'})	-- LSP REQUIREMENTS 
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = 'TSUpdate'})	-- Syntax Parser
+Plug('williamboman/mason.nvim')									-- LSP REQUIREMENTS
+Plug('williamboman/mason-lspconfig.nvim')						-- LSP REQUIREMENTS
+Plug('neovim/nvim-lspconfig')									-- LSP REQUIREMENTS
+Plug('hrsh7th/nvim-cmp')										-- LSP REQUIREMENTS
+Plug('hrsh7th/cmp-nvim-lsp')									-- LSP REQUIREMENTS
+Plug('L3MON4D3/LuaSnip')										-- LSP REQUIREMENTS
+Plug('VonHeikemen/lsp-zero.nvim', {['branch'] = 'v3.x'})		-- LSP REQUIREMENTS 
 -------------------------------------------------------------------------------
 vim.call('plug#end')
 -------------------------------------------------------------------------------
-
------------------------------------------------------
+-------------------------------------------------------------------------------
 -- COMMENT THIS LINE AFTER THE FIRST RUN of :PlugInstall 
 if true then return end
-------------------------------------------------------
-
+-------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 -- PLUGIN CONFIGURATION
 -------------------------------------------------------------------------------
+vim.g.lightline = {  colorscheme = 'wombat' }
+-------------------------------------------------------------------------------
 -- TOGGLE TERM 
 require("toggleterm").setup()
--------------------------------------------------------------------------------
--- TOKYO NIGHT THEME CONFIGURATION
-local bg = "#011628"
-local bg_dark = "#011423"
-local bg_highlight = "#143652"
-local bg_search = "#0A64AC"
-local bg_visual = "#275378"
-local fg = "#CBE0F0"
-local fg_dark = "#B4D0E9"
-local fg_gutter = "#627E97"
-local border = "#547998"
-
-require("tokyonight").setup({
-  style = "night",
-  transparent = transparent,
-  styles = {
-    sidebars = transparent and "transparent" or "dark",
-    floats = transparent and "transparent" or "dark",
-  },
-  on_colors = function(colors)
-    colors.bg = bg
-    colors.bg_dark = transparent and colors.none or bg_dark
-    colors.bg_float = transparent and colors.none or bg_dark
-    colors.bg_highlight = bg_highlight
-    colors.bg_popup = bg_dark
-    colors.bg_search = bg_search
-    colors.bg_sidebar = transparent and colors.none or bg_dark
-    colors.bg_statusline = transparent and colors.none or bg_dark
-    colors.bg_visual = bg_visual
-    colors.border = border
-    colors.fg = fg
-    colors.fg_dark = fg_dark
-    colors.fg_float = fg
-    colors.fg_gutter = fg_gutter
-    colors.fg_sidebar = fg_dark
-  end,
-})
 -------------------------------------------------------------------------------
 -- BUFFER LINE 
 vim.opt.termguicolors = true
@@ -127,16 +88,15 @@ cmp.setup({
 })
 -----------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------
------------------------------------------------------------------------------------
 -- VIM SETTINGS
 -----------------------------------------------------------------------------------
-vim.cmd('colorscheme tokyonight')						-- COLORSCHEMA
-vim.api.nvim_set_hl(0, "Normal", {bg = "none"})			-- TRANSPARENT BACKGROUND
-vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})	-- TRANSPARENT BACKGROUND
+vim.cmd('colorscheme vscode')							-- COLORSCHEMA
+-- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})		-- TRANSPARENT BACKGROUND
+-- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})	-- TRANSPARENT BACKGROUND
 -----------------------------------------------------------------------------------
-vim.cmd('set number')
 vim.cmd('set tabstop=4')
 vim.cmd('set softtabstop=4')
+vim.cmd('set number')
 vim.cmd('set shiftwidth=4')
 vim.cmd('set autoindent')
 vim.cmd('set smartindent')
@@ -147,6 +107,7 @@ vim.cmd('set noswapfile')
 vim.cmd('set nobackup')
 vim.cmd('set showcmd')
 vim.cmd('set scrolloff=10')
+vim.cmd('set t_Co=256')
 -----------------------------------------------------------------------------------
 -- KEYMAPS
 -----------------------------------------------------------------------------------
@@ -186,13 +147,11 @@ vim.api.nvim_set_keymap('n', '<M-1>', ':ToggleTerm 1<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<M-2>', ':ToggleTerm 2<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<M-3>', ':ToggleTerm 3<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<M-4>', ':ToggleTerm 4<CR>', {noremap = true})
-
 vim.api.nvim_set_keymap('i', '<C-t>', '<Esc>:ToggleTerm<CR>', {noremap = true})
 vim.api.nvim_set_keymap('i', '<M-1>', '<Esc>:ToggleTerm 1<CR>', {noremap = true})
 vim.api.nvim_set_keymap('i', '<M-2>', '<Esc>:ToggleTerm 2<CR>', {noremap = true})
 vim.api.nvim_set_keymap('i', '<M-3>', '<Esc>:ToggleTerm 3<CR>', {noremap = true})
 vim.api.nvim_set_keymap('i', '<M-4>', '<Esc>:ToggleTerm 4<CR>', {noremap = true})
-
 vim.api.nvim_set_keymap('t', '<C-t>', [[<C-\><C-n><C-t>]], {noremap = true})
 vim.api.nvim_set_keymap('t', '<M-1>', [[<C-\><C-n><C-t>]], {noremap = true})
 vim.api.nvim_set_keymap('t', '<M-2>', [[<C-\><C-n><C-t>]], {noremap = true})
